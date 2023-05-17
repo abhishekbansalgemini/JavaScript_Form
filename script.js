c199d096da2703fc71d7f8e964ba3e932cfeb838
Abhishek Bansal1 <Abhishek.Bansal1@Geminisolutions.com>
2023-05-17 11:20:38 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,103 @@
+function validate(e){
+    e.preventDefault();
+    var passValue = document.getElementById("password").value
+    var confpassValue = document.getElementById("confirm_password").value
+    if(passValue !== confpassValue) {
+       window.alert("Passwords does not match!");
+    }  
+    
+   
+    // Validating Name
+
+    var first_name = document.getElementById("first_name").value;
+    for(let i=0; i<first_name.length; i++){
+        if((first_name[i] >= 'a' && first_name[i] <= 'z') || (first_name[i] >= 'A' && first_name[i] <= 'Z') || (first_name[i] === ' '))  {
+
+        }  
+        else{
+            window.alert("Enter the valid name");
+            break;
+        }
+    }
+
+    var last_name = document.getElementById("last_name").value;
+    for(let i=0; i<last_name.length; i++){
+        if((last_name[i] >= 'a' && last_name[i] <= 'z') || (last_name[i] >= 'A' && last_name[i] <= 'Z') || (last_name[i] === ' '))  {
+            
+        }  
+        else{
+            window.alert("Enter the valid name");
+            break;
+        }
+    }
+
+
+    //validating mobile
+
+
+    var mobile = document.getElementById("phone").value;
+    if(mobile.length != 10){
+        window.alert("Invalid Phone Number");
+    }
+    else if(mobile[0] >='0' && mobile[0] <= '5'){
+        window.alert("Invalid Phone Number");
+    }
+    else{
+        for(let i=0; i<10; i++){
+            if(mobile[i] >= '0' && mobile[i] <= '9'){
+
+            }
+            else{
+                window.alert("Invalid Phone Number");
+                break;
+            }
+        }
+    }
+
+    if(flag3 === false){
+        mob = mobile
+    }
+
+
+    //table logic
+
+    // var table = document.createElement("table");
+    // var div = document.getElementById("mytable");
+    // div.appendChild(table);
+    // table.id = "user_table";
+    // table.style.borderCollapse = "collapse";
+
+    // //creating Caption
+
+    // var caption = document.createElement('caption');
+    // var cap = document.createTextNode("User Details");
+    // caption.appendChild(cap);
+    // table.appendChild(caption);
+
+    // const fields = ["First_Name", "Last_Name", "Phone", "Password"];
+
+    // var thead = document.createElement("thead");
+    // for (let i = 0; i < 4; i++) {
+    //     let th = document.createElement("th");
+    //     let h = document.createTextNode(fields[i]);
+    //     th.appendChild(h);
+    //     th.style.border = "2px solid black";
+    //     th.style.textAlign = "center";
+    //     thead.append(th);
+    // }
+    // table.appendChild(thead)
+    // // console.log(div);
+    // var tbody = document.createElement("tbody");
+    // if(fname !== null && lname !== null && pass != null &&  mob != null){
+    //     const row = [fname, lname, mob, pass];
+    //     for (let i = 0; i < row.length; i++) {
+    //         let td = document.createElement("td");
+    //         let d = document.createTextNode(rows[i]);
+    //         td.style.border = "2px solid black"
+    //         td.appendChild(d);
+    //         td.style.padding = '10px';
+    //         td.style.textAlign = "center";
+    //         tbody.append(td);
+    //     }
+    // }
+}
